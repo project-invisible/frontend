917cdfcf4209fab26b86392aed2586e4ee927ab9
Veronika <veronika.babic@web.de>
2020-01-08 14:25:01 +0100

    close detailView on search

--- src/map/SearchBar.tsx
+++ src/map/SearchBar.tsx
@@ -63,6 +63,7 @@ export default function SearchBar() {
   const handleClick = event => {
     event.preventDefault();
     dispatch(searchPOI(searchQuery));
+    dispatch(closeDetailView(false));
   };
 
   const search = (
