e64d527759df94597578fa312ca044c749490293
Veronika <veronika.babic@web.de>
2020-01-08 14:27:47 +0100

    adjusted conditions open and close detailView

--- src/map/HomeMap.tsx
+++ src/map/HomeMap.tsx
@@ -1,13 +1,14 @@
-import React, { Component, useEffect, useRef } from 'react'
-import { Map, Marker, Popup, TileLayer, Pane, ZoomControl, LayerGroup, Circle, useLeaflet } from 'react-leaflet'
-import { LatLngExpression } from 'leaflet'
-import { makeStyles } from '@material-ui/core/styles';
-import SearchBar from './SearchBar';
-import Control from 'react-leaflet-control';
-import SwitchLayer from './SwitchLayer';
-import { useSelector, useDispatch } from 'react-redux';
-import { getDetails } from "./DetailsReducer";
-import PointsOfInterest from './PointsOfInterest';
+import React, { useRef } from "react";
+import {
+  Map,
+  TileLayer,
+  ZoomControl} from "react-leaflet";
+import { LatLngExpression } from "leaflet";
+import { makeStyles } from "@material-ui/core/styles";
+import SearchBar from "./SearchBar";
+import Control from "react-leaflet-control";
+import SwitchLayer from "./SwitchLayer";
+import PointsOfInterest from "./PointsOfInterest";
 
 const useStyles = makeStyles({
   root: {
@@ -20,69 +21,40 @@ const useStyles = makeStyles({
   }
 });
 
-const center: LatLngExpression = [52.505, -0.09];
-
 export default function HomeMap() {
   const classes = useStyles({});
-const dispatch = useDispatch();
 
-  const showDetails: boolean = useSelector(
-    (state: any) => state.detailsStore.showDetails
-  );
-
-  const id: number = useSelector(
-    (state:any) => state.detailsStore.id
-  );
-  console.log(id);  
-const childRef = useRef(null);
+  const childRef = useRef(null);
 
   const mapAttributes = {
     lat: 52.5137,
     lng: 13.322,
-    zoom: 8,
-  }
-  const position: LatLngExpression = [mapAttributes.lat, mapAttributes.lng]
+    zoom: 8
+  };
+  const position: LatLngExpression = [mapAttributes.lat, mapAttributes.lng];
 
-  const markers: Array<LatLngExpression> = useSelector(
-    (state: any) => state.switchLayerStore.mapMarker
+  return (
+    <>
+      <Map
+        center={position}
+        zoom={mapAttributes.zoom}
+        className={classes.root}
+        zoomControl={false}
+        onMoveEnd={() => childRef.current.callUpdate()}
+      >
+        <ZoomControl position="bottomright" />
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Control position="topleft">
+          <SearchBar />
+        </Control>
+        <Control position="topleft">
+          <SwitchLayer />
+        </Control>
+        <PointsOfInterest ref={childRef} />
+      </Map>
+    </>
   );
-
-    return (
-      <>
-        <Map center={position} zoom={mapAttributes.zoom} className={classes.root} zoomControl={false} onMoveEnd={() => childRef.current.callUpdate()}>
-          <ZoomControl position="bottomright"/>
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Control position="topleft" >
-              <SearchBar />
-          </Control>
-          <Control position="topleft" >
-              <SwitchLayer />
-          </Control>
-          <LayerGroup>
-            {
-              markers ?
-              markers.map( marker => {
-                return (
-                  <Marker position={marker}
-                  onClick={() =>
-                    !showDetails
-                      ? dispatch(getDetails(true, id))
-                      : dispatch(getDetails(false, id))
-                  }>
-                    <Popup>
-                      A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                  </Marker>
-                )
-              })
-              : <div/>
-            }
-          </LayerGroup>
-          <PointsOfInterest ref={childRef}/>
-        </Map>
-      </>
-    )
 }
