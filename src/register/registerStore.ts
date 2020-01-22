b888f36409d075c5ad4aa48369bfc2c189a240f0
Unknown <simon.binder.94@gmail.com>
2020-01-22 11:56:18 +0100

    Add token validation

--- src/register/registerStore.ts
+++ src/register/registerStore.ts
@@ -1,54 +1,76 @@
+import { Role } from "../types/User";
 
-import { createStore } from 'redux';
-
-export const REGISTER = 'REGISTER'
+export const REGISTER = "REGISTER";
+export const REGISTER_ERROR = "REGISTER_ERROR";
 
 const initialState = {
   authenticated: false,
-  jwt: '',
+  email: "",
+  userGroup: Role,
+  token: "",
+  error: ""
 };
 
-  /**
-   * Reducer
-   */
-  const registerStore = (state = initialState, action) => {
-    switch (action.type) {
-      case REGISTER:
-        state.authenticated = true;
-        state.jwt = action.jwt;
-        return state;
-    }
-    return state;
+/**
+ * Reducer
+ */
+const registerStore = (state = initialState, action) => {
+  switch (action.type) {
+    case REGISTER:
+      state.authenticated = true;
+      state.email = action.result.email;
+      state.userGroup = action.result.group;
+      state.token = action.result.token;
+      state.error = "";
+      return state;
+    case REGISTER_ERROR:
+      state.error = "Error: User already registered!";
+      return state;
   }
-  
-  /**
+  return state;
+};
+
+/**
  * Actions
  */
-export const postRegistration = (email: String, username: String, password: String) => async (dispatch, getState) => {
-    const body: BodyInit = JSON.stringify({
-        email,
-        password,
-        username
-    });
-    try {
-        const response = await fetch('http://localhost:8182/authenticate/register', {
-          method: 'post',
-          headers: {'Content-Type':'application/json'},
-          body
-         });
-        console.log(response);
-        const jwt = "test";
-        return {
-            type: REGISTER,
-            jwt,
-        }
-    } catch (error) {
-        console.log("throwing Error", error);
-        throw error;
+export const postRegistration = (
+  email: String,
+  username: String,
+  password: String
+) => async (dispatch, getState) => {
+  const body: BodyInit = JSON.stringify({
+    email,
+    password,
+    username
+  });
+  try {
+    const response = await fetch(
+      "http://localhost:8182/authenticate/register",
+      {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body
+      }
+    );
+    const result = await response.json();
+    console.log(result);
+    if (result.exists === true) {
+      dispatch({
+        type: REGISTER,
+        result
+      });
+    } else {
+      dispatch({
+        type: REGISTER_ERROR
+      });
     }
-  };
+  } catch (error) {
+    console.log("throwing Error", error);
+    throw error;
+  }
+};
 
-  /**
-   * Store
-   */
-  export default registerStore;
+/**
+ * Store
+ */
+export default registerStore;
