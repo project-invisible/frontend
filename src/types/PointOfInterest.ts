c3341e698262ec154feb84cd1f10762f7247813c
Unknown <simon.binder.94@gmail.com>
2020-01-22 10:53:08 +0100

    Adjust frontend model to new rating system

--- src/types/PointOfInterest.ts
+++ src/types/PointOfInterest.ts
@@ -1,15 +1,22 @@
+import { Question } from './Rating';
+
 export interface PointOfInterest {
     id: number
     name: String,
-    overallRating: number,
     street: string,
     postal: number,
     description: string,
     email: string,
     website: string,
     city: string,
+    overallRatingPerQuestion: QuestionRatingObject[],
     coordinates: {
         x: number,
         y: number,
     }
 }
+
+export interface QuestionRatingObject {
+    question: Question,
+    rating: number,
+  }
