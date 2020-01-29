0728d253b3ba66a9e2b5bd8fb08eae2e98cb2c11
Unknown <simon.binder.94@gmail.com>
2020-01-29 16:15:18 +0100

    Add docker deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-set -xe
 
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-  eval "$(ssh-agent -s)"
-  ssh-add
-  npm run build
-  rsync -rq --delete --rsync-path="mkdir -p frontend && rsync" \
-  $TRAVIS_BUILD_DIR/public travis@invisible.f4.htw-berlin.de:frontend
-  echo $TRAVIS_BUILD_DIR
+  echo "--- SSH"
+  ssh -o StrictHostKeyChecking=no travis@invisible.f4.htw-berlin.de "pwd  ; exit"
+  echo "--- Building docker file"
+  docker build -f Dockerfile -t invisibleproject/in_visible:frontend .
+
+  echo "--- docker images ---"
+  docker images
+
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+  docker push invisibleproject/in_visible:frontend
+
+  scp -o StrictHostKeyChecking=no docker-compose.yml travis@invisible.f4.htw-berlin.de:~
+  
+  echo "+++++ stopping docker containers"
+  ssh -t -o StrictHostKeyChecking=no travis@invisible.f4.htw-berlin.de 'docker-compose down'
+  
+  echo "+++++ pulling and starting docker containers"
+   ssh -t -o StrictHostKeyChecking=no travis@invisible.f4.htw-berlin.de 'docker-compose up -d'
 else
   echo "Not deploying, since this branch isn't master"
 fi
